00653b9d1ab83839d20d57f89cdae7377a7b6c6b
mehdi <mehdiarghan@gmail.com.git/>
2023-09-14 21:28:56 +0330

    added package Connection and class HibernateUtil

---
+++ src/main/java/Connection/HibernateUtil.java
@@ -0,0 +1,18 @@
+package Connection;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    public static final StandardServiceRegistry STANDARD_SERVICE_REGISTRY = new StandardServiceRegistryBuilder()
+            .configure("hibernate.cfg.xml").build();
+    public static final Metadata METADATA = new MetadataSources(STANDARD_SERVICE_REGISTRY).getMetadataBuilder().build();
+    public static final SessionFactory SESSION_FACTORY = METADATA.getSessionFactoryBuilder().build();
+
+    public SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+}
